c15a83c341f00fb7af2ef7e31f6707bd018d4e60
Jona <jonathanelian64@gmail.com>
2022-11-28 23:10:42 -0600

    Add static files to server

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,36 @@ import router from "./routes/users.ts";
 const app = new Application();
 const port = 3000;
 
+// app.use(async (context: Context, next) => {
+//   const root = `${Deno.cwd()}/static`
+//   try {
+//     await context.send({ root })
+//   } catch {
+//     next()
+//   }
+// })
+
 app.use(router.routes());
 
-app.use((ctx: Context) => {
-  ctx.response.body = "Hola Deno 🐱‍🐉";
-  ctx.response.status = 200;
+app.use(async (ctx: Context) => {
+  try {
+    await ctx.send({
+      root: `${Deno.cwd()}/public`,
+      index: "index.html",
+    });
+  } catch {
+    ctx.response.status = 404;
+    ctx.response.body = "404 File not found";
+  }
 });
 
-console.log(`Server running in http://localhost:${port} 🐢`);
+
+
+// app.use((ctx: Context) => {
+//   ctx.response.body = "Hola Deno 🐱‍🐉";
+//   ctx.response.status = 200;
+// });
+
+
 
 await app.listen({ port });
