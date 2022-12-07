21fbcf657b450165dbb4adc44ab5fd5b055e7e3c
Jona <jonathanelian64@gmail.com>
2022-12-06 21:44:38 -0600

    move callback to controllers getUsers

---
+++ src/controllers/users.ts
@@ -0,0 +1,17 @@
+import { Context } from "../../deps.ts";
+import { User } from "../user.ts";
+
+const getUsers = (context: Context) => {
+  const usuario: User = {
+    name: "Fernando",
+    age: 36,
+    lang: "Javascript",
+  };
+  context.response.status = 200;
+  context.response.body = usuario;
+}
+
+export {
+  getUsers
+
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,17 +1,10 @@
 import { Router } from "../../deps.ts";
-import { User } from "../user.ts";
+import { getUsers } from "../controllers/users.ts";
+
 
 const routerUsers = new Router();
 
 routerUsers
-  .get("/users", (context) => {
-    const usuario: User = {
-      name: "Fernando",
-      age: 36,
-      lang: "Javascript",
-    };
-    context.response.status = 200;
-    context.response.body = usuario;
-  });
+  .get("/users", getUsers);
 
 export default routerUsers;
